981fa46febdc8ed441e782b44ae645d72a5f485f
incudaste <150724839+incudaste@users.noreply.github.com>
2024-03-04 14:48:21 +0300

    Add files via upload

---
+++ L4_WhileLoop.java
@@ -0,0 +1,13 @@
+package lessons;
+
+class L4_WhileLoop {
+	
+	public static void main(String[] args) {
+		int counter = 0;
+		
+		while(counter < 10) {
+			counter++;
+			System.out.println("Iteration №" + counter);
+		}
+	}
+}
