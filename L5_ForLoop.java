36bbcb6423429e15b320a07fba21a472295528d3
incudaste <150724839+incudaste@users.noreply.github.com>
2024-03-04 15:14:24 +0300

    Add files via upload

---
+++ L5_ForLoop.java
@@ -0,0 +1,16 @@
+package lessons;
+
+class L5_ForLoop {
+	
+	public static void main(String[] args) {
+		for(int counter = 1; counter <= 10; counter++) {
+			System.out.println("Iteration №" + counter);
+		}
+		
+		System.out.println();
+		
+		for(int counter = 10; counter >= 1; counter--) {
+			System.out.println("Iteration №" + counter);
+		}
+	}
+}
