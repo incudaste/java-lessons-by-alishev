8e96a79a41004917688dd5cdfdfb9e3004678fdd
incudaste <150724839+incudaste@users.noreply.github.com>
2024-03-04 13:49:39 +0300

    Add files via upload

---
+++ L2_VariablesAndPrimitives.java
@@ -0,0 +1,22 @@
+package lessons;
+
+class L2_VariablesAndPrimitives {
+	
+	public static void main(String[] args) {
+		byte smallestNumber = -104;
+		short shortNumber = 2539;
+		int integer = -692836;
+		long largestNumber = 31054636L;
+		
+		float floatNumber = 72.24f;
+		double doubleNumber = 1247.853335d;
+		
+		var type1 = 130;
+		var type2 = 3.14f;
+		var type3 = '\u0024';
+		
+		boolean premiumAvailability = true;
+		
+		char euro = '\u20ac';
+	}
+}
