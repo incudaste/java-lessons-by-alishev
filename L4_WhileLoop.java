98c1849d67eee241aca04b94db4a980fe9f24b57
incudaste <150724839+incudaste@users.noreply.github.com>
2024-03-04 18:49:12 +0300

    Update L4_WhileLoop.java

--- L4_WhileLoop.java
+++ L4_WhileLoop.java
@@ -5,7 +5,7 @@ class L4_WhileLoop {
 	public static void main(String[] args) {
 		int counter = 0;
 		
-		while(counter < 10) {
+		while (counter < 10) {
 			counter++;
 			System.out.println("Iteration №" + counter);
 		}
