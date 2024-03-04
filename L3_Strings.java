6c67c0e9d73b5f7fa467a98d35e2552da1bbda87
incudaste <150724839+incudaste@users.noreply.github.com>
2024-03-04 14:26:12 +0300

    Add files via upload

---
+++ L3_Strings.java
@@ -0,0 +1,18 @@
+package lessons;
+
+class L3_Strings {
+	
+	public static void main(String[] args) {
+		String part1 = new String("I live ");
+		String part2 = "in Russia!";
+		
+		System.out.println(part1 + part2);
+		
+		String americanCompanyName
+			= new String("Advanced Micro Devices");
+		String germanCompanyName = "Volkswagen";
+		
+		System.out.println(americanCompanyName
+		                  + " & " + germanCompanyName);
+	}
+}
